84ca69efde15b426e72ae4f7edc300dc493cc679
bekboloffisial <tbkg1558@gmail.com>
2022-07-03 17:17:12 +0600

    list

--- src/App.js
+++ src/App.js
@@ -1,23 +1,123 @@
-import logo from './logo.svg';
 import './App.css';
+import {useEffect, useState} from "react";
+import TodoList from "./TodoList/TodoList";
+import axios from "axios";
 
 function App() {
+
+    const [todo, setTodo] = useState([])
+    const [title, setTitle] = useState("")
+
+
+    const handleChange = (e) => {
+        setTitle(e.target.value)
+    }
+
+    const addTodo = () => {
+        const newTodo ={
+            id: todo.length? todo[todo.length -1].id +1 : 1,
+            name: title,
+            isDone:false
+        }
+
+        // setTodo((todo) => [...todo, newTodo])
+
+        axios.post(`https://62c17235eff7f7856f0ec450.mockapi.io/frontend/todo`, {
+            name: title,
+        })
+            .then(({data}) => {
+                console.log(data)
+            })
+    }
+
+    const deleteTodo = (id) => {
+        setTodo(state => state.filter(el => el.id !== id ))
+        axios.delete(`https://62c17235eff7f7856f0ec450.mockapi.io/frontend/todo/${id}`)
+            .then(({data}) => {
+                setTodo([...todo, data])
+            })
+    }
+
+    const changeStatus = (id) => {
+        setTodo(state => state.map(el => el.id === id ? {...el, isDone:  !el.isDone} : el))
+    }
+
+    const updateTodo = (title, id) => {
+        setTodo(state => state.map(el => el.id === id ? {...el, name: title} : el))
+    }
+
+    const getTodo = async () => {
+        const url = await axios("https://62c17235eff7f7856f0ec450.mockapi.io/frontend/todo")
+        const {data} = await url
+        setTodo(data)
+        return;
+    }
+
+
+    useEffect(() => {
+       getTodo()
+    })
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+          <h1 className="text-4xl my-6">TODO APP</h1>
+          <div className="flex-row flex flex-wrap justify-center">
+              <div className="basis-1/3 justify-center items-center">
+
+                  <div className="">
+                      <div className="flex">
+                          <input
+                              onChange={handleChange}
+                              id="default-search"
+                                 className="w-full mr-2 p-3 text-sm text-white bg-gray-700 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                 placeholder="type todo name" required/>
+                          <button
+                              onClick={addTodo}
+                              className="text-white  bg-blue-700
+                                            hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4  dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                              добавить
+                          </button>
+                      </div>
+
+                      <ul className="my-4 text-sm font-medium text-white bg-gray-700 rounded-lg dark:bg-gray-700 dark:border-gray-600 dark:text-white">
+
+                          {
+                              todo.filter(el => el.isDone === false).map(el => <TodoList
+                                  changeStatus={changeStatus}
+                                  deleteTodo={deleteTodo}
+                                  el={el}
+                                  key={el.id}
+                                  updateTodo={updateTodo}
+                              />)
+                          }
+
+                      </ul>
+                  </div>
+
+              </div>
+
+          </div>
+
+          <div className="flex flex-wrap flex-row justify-center py-16">
+              <div className="basis-1/3">
+                  <hr/>
+                  Законченные дела!
+                  <hr/>
+
+                  <ul className="my-4 text-sm font-medium text-white bg-gray-700 rounded-lg dark:bg-gray-700 dark:border-gray-600 dark:text-white">
+
+                      {
+                          todo.filter(el => el.isDone === true).map(el => <TodoList changeStatus={changeStatus} deleteTodo={deleteTodo} el={el} key={el.id}/>)
+                      }
+
+                  </ul>
+
+              </div>
+          </div>
+
+      </div>
     </div>
   );
 }
---
+++ src/TodoList/TodoList.js
@@ -0,0 +1,55 @@
+import React, {useState} from 'react';
+
+const TodoList = ({el, deleteTodo, changeStatus, updateTodo}) => {
+
+    const [isOpen, setIsOpen] = useState(false)
+    const [title, setTitle] = useState(el.name)
+
+    const openInput = () => {
+        setIsOpen(true)
+    }
+
+    const closeInput = (title, id) => {
+        updateTodo(title,id)
+        setIsOpen(false)
+    }
+
+    const saveChange = (e) => setTitle(e.target.value)
+
+
+    return (
+        <li className=" px-4 py-2 border-b border-gray-200 rounded-t-lg dark:border-gray-600 flex items-center justify-between">
+                                     <span className="flex">
+                                          <input
+                                              onChange={() => changeStatus(el.id)}
+
+                                              checked={el.isDone}
+                                              type="checkbox"/>
+                                         {
+                                             isOpen ? <input className="text-black p-1 mx-2 bg-yellow-100"
+                                                             onChange={saveChange}
+                                                             type="text"
+                                                             value={title}/> :   <span
+                                                 style={{
+                                                     textDecoration: el.isDone? "line-through": ""
+                                                 }}
+                                                 className="mx-2">{el.name}</span>
+                                         }
+                                     </span>
+            <span className="flex items-center">
+                                          <button
+                                              onClick={() => isOpen? closeInput(title, el.id) : openInput()}
+                                                  className="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">
+                                              {isOpen? "сохранить" : "изменить"}</button>
+
+                                          <button
+                                              onClick={() => deleteTodo(el.id)}
+                                              type="button"
+                                                  className="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900">
+                                              удалить</button>
+                                      </span>
+        </li>
+    );
+};
+
+export default TodoList;
